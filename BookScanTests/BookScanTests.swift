bf39797f9a7d5f10a8ef526515c6a940970915c1
Marian Mihailescu <mihailescu2m@gmail.com>
2026-01-29 18:44:25 +1030

    Initial Commit

---
+++ BookScanTests/BookScanTests.swift
@@ -0,0 +1,17 @@
+//
+//  BookScanTests.swift
+//  BookScanTests
+//
+//  Created by Marian Mihailescu on 29/1/2026.
+//
+
+import Testing
+@testable import BookScan
+
+struct BookScanTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
